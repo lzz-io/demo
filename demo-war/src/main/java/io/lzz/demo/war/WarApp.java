04ce87963d29eb3609fbf4b4cdc014a09af24c53
lzz.io <1219331697@qq.com>
2019-12-04 01:15:10 +0800

    demo war spring 注解

---
+++ demo-war/src/main/java/io/lzz/demo/war/WarApp.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) qq:1219331697
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.lzz.demo.war;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import io.lzz.demo.war.config.AppConfig;
+
+/**
+ * @author q1219331697
+ *
+ */
+public class WarApp implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+
+		// Load Spring web application configuration
+		AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+		ac.register(AppConfig.class);
+		ac.refresh();
+
+		// Create and register the DispatcherServlet
+		DispatcherServlet servlet = new DispatcherServlet(ac);
+		ServletRegistration.Dynamic registration = servletContext.addServlet("app", servlet);
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/");
+
+	}
+
+}
---
+++ demo-war/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>index</title>
+<base href="<%=request.getContextPath()%>">
+</head>
+<body>
+	<h1>index.jsp</h1>
+
+	<p>
+		<a href="<%=request.getContextPath()%>/index">spring index
+			controller</a>
+	</p>
+</body>
+</html>
