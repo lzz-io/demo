72599360d5516ced4995836a8f6aab24d707c288
q1219331697 <1219331697@qq.com>
2024-09-08 13:44:11 +0800

    JobBeanPostProcessor

---
+++ demo-spring-batch/src/main/java/io/lzz/demo/spring/batch/config/JobBeanPostProcessor.java
@@ -0,0 +1,32 @@
+package io.lzz.demo.spring.batch.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.job.AbstractJob;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JobBeanPostProcessor implements BeanPostProcessor, JobExecutionListener {
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof AbstractJob) {
+            ((AbstractJob) bean).registerJobExecutionListener(this);
+        }
+        return bean;
+    }
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        log.info("job[{}]开始", jobExecution.getJobInstance().getJobName());
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        log.info("job[{}]结束,执行时间[{}]", jobExecution.getJobInstance().getJobName(),
+                jobExecution.getEndTime().getTime() - jobExecution.getStartTime().getTime());
+    }
+}
